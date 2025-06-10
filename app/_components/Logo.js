ad6985a151dc8ff0a0842ae2bfe58f89edda8395
Dmitry <superflick3@gmail.com>
2025-06-10 14:57:31 +0300

    Add homepage, styling

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
-
+// import logo from '@/public/logo.png';
 function Logo() {
     return (
         <Link href="/" className="flex items-center gap-4 z-10">
             <Image
                 src="/logo.png"
-                height="60"
+                height="80"
                 width="80"
                 alt="The Wild Oasis logo"
                 priority
+                className="scale-x-95"
             />
+
             <span className="text-xl font-semibold text-primary-100">
                 The Oasis
             </span>

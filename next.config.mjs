bccab937444c706d0cf8261c9273f6f08a3adc75
Dmitry <superflick3@gmail.com>
2025-06-16 11:59:47 +0300

    Add cabins cards

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            new URL(
+                'https://flledracehzjzjggtmnk.supabase.co/storage/v1/object/public/cabin-images/**'
+            ),
+        ],
+    },
+};
 
 export default nextConfig;
